5bd35e39094de81a955a96a2dc12db1c86224867
marvin1604 <walter.rodriguez1992@gmail.com>
2022-12-16 11:54:51 -0500

    deploy del proyecto

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./Card.css"
 
-import web from "../../assest/nuevos/browser-website-svgrepo-com.svg"
 
 const Card = ({nombre,descripcion,url,imagen,github}) => {
   return (
